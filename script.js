a6ba3041b7cf1e8f2965ec266b67749bc27fa69f
fawzyiiyad <hfwzy180@gmail.com>
2025-01-09 05:05:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -101,11 +101,11 @@ style.innerHTML = `
     to {
         transform: translateY(-50px) scale(1.5);
         opacity: 0;
-    }
+}`;
+  }
     function openPopup(event, url) {
     event.preventDefault(); // منع السلوك الافتراضي (فتح الرابط مباشرة)
     window.open(url, "popupWindow", "width=690,height=650,resizable=no,scrollbars=yes");
   }
-}`;
 document.head.appendChild(style);
 
